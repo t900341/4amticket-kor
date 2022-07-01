2a67a3a84c15ea08ff35c3c9888f3d5af18a4944
unknown <t900341@gmail.com>
2022-07-01 16:57:53 +0900

    first commit

--- server/connection.php
+++ server/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$conn = mysqli_connect("database-2.cqywbbvmrjx8.ap-northeast-2.rds.amazonaws.com","admin","","shopping_korea")
+$conn = mysqli_connect("final-test.czhjipeoxdi3.ap-northeast-2.rds.amazonaws.com","admin","password","shopping_korea")
         or die("Couldn't connect to database");
 
 
